b34951663fe7ca4899536e04274b63fd4df901d9
Sakil-Dayama <sakilmohammaddayama>
2022-12-22 20:52:59 +0530

    22-12

--- src/screens/before_login/Login.js
+++ src/screens/before_login/Login.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
 import React from 'react'
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { useIsFocused } from '@react-navigation/native'
 import { FULL_HEIGHT, FULL_WIDTH, RADIUS } from '../../constants/layout'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -12,59 +12,114 @@ import UiButton from '../../components/UI/UiButton'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import ViewContainer from '../../components/HOC/ViewContainer';
 import ScrollContainer from '../../components/HOC/ScrollContainer';
-const Login = ({ navigation, }) => {
+import { isValidForm, validators } from '../../constants/validation';
+import { POST } from '../../backend/Backend'
+import Toast from 'react-native-simple-toast';
 
-  
+const Login = ({ navigation }) => {
 
-  // let token = 'demo_token'
-  // const setlogins = async () => {
+  const [mobile_number, setMobileNumber] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, seterrors] = useState('')
+  const [loading, setLoading] = useState(false)
 
-  //   await AsyncStorage.setItem('TOKEN', token)
-  //   navigation.reset({
-  //     index: 0,
-  //     routes: [{ name: 'Wellcom' }]
-  //   })
-  // }
-  // useEffect(() => {
-  //   setlogins()
-  // }, [])
+  const onLogin = async () => {
+    const form = {
+      mobile_number: validators.checkNumber('mobile_number', mobile_number),
+      password: validators.checkPassword('Password', password),
+    }
+    seterrors(form);
+    if (isValidForm(form)) {
+      let body = {
+        mobile_number: mobile_number,
+        device_information: {
+          device_id: "12345",
+          os_name: "Android",
+          model_name: "SM-M307F",
+          os_version: "10",
+          app_version: "1.3.0",
+          manufacturer: "samsung",
+          total_memory: "5860327424",
+          fcm_token: "12345"
+        }
+      }
+      console.log('body-------->', body);
+      POST(
+        "http://54.144.109.80:5000/api/v1/login",
+        body,
+        { 'Authorization': 'Basic YWRtaW46YWRtaW4=' },
+        res => {
+          console.log('res==>', res)
+          if (res.ok) {
+            Toast.showWithGravity('okkkk', Toast.LONG, Toast.TOP);
+            navigation.navigate("Home")
+          } else {
+            Toast.showWithGravity('not ok', Toast.LONG, Toast.TOP);
+          }
+        },
+        err => {
+          console.log('err---->', err)
+          let error = JSON.parse(err.res1);
+          Toast.showWithGravity(error.message, Toast.LONG, Toast.TOP);
+        },
+        fail => {
+          console.log('fail==>', fail)
+          Toast.showWithGravity('Api failed');
+        }
+      )
+
+      // let result = await fetch('http://54.144.109.80:5000/api/v1/signup', {
+      //   method: 'post',
+      //   body: body,
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'Authorization': 'Basic YWRtaW46YWRtaW4='
+      //   }
+      // })
+      // console.log('res===>', await result.json());
+
+    } else {
+    }
+  }
+
   return (
-    <ViewContainer style={{flex:1,backgroundColor:colors.darkcyan}}>
+    <ViewContainer style={{ flex: 1, backgroundColor: colors.darkcyan }}>
       <ScrollContainer>
-    
-          <View style={styles.BoxONEContainer}>
-            <Image style={styles.ImagePlate} source={icons.mixxchaimin} />
-          </View>
-          <View style={styles.BoxTWOContainer}>
-            <View>
-              <Paragraph style={styles.LoginText}>Login</Paragraph>
-            </View>
-            <Input style={{ backgroundColor: '#fff5ee' }} placeholder='Email' />
-           <Input style={{ backgroundColor: '#fff5ee' }} placeholder='Password' />
-           <View style={styles.lock_VIEW} >
-              <Image  style={styles.lock_image} source={icons.lock} />
-           </View>
 
-            <View style={styles.forgat_Text}>
-              <Paragraph>Forgat Password</Paragraph>
-            </View>
-            <View style={styles.Ematy_Container}></View>
-            <View style={styles.OR_VIEW}>
-              <Paragraph size={12}>OR</Paragraph>
-            </View>
-            <View style={styles.IconsVIEW} >
-              <Image style={styles.facebookImage} source={icons.facebook} />
-              <Image style={styles.googleImage} source={icons.google} />
-            </View>
-            <UiButton text='LOGIN'
-              style={styles.UiButton}
-             
-            />
-            <View style={styles.Register_red}>
-              <Paragraph>Don't have an account ?<Paragraph   onPress={() => navigation.navigate('SignUp')} color={'red'} style={styles.Register_red}> Register</Paragraph></Paragraph>
-            </View>
+        <View style={styles.BoxONEContainer}>
+          <Image style={styles.ImagePlate} source={icons.mixxchaimin} />
+        </View>
+        <View style={styles.BoxTWOContainer}>
+          <View>
+            <Paragraph style={styles.LoginText}>Login</Paragraph>
           </View>
-      
+          <Input onChange={setMobileNumber} error={errors?.mobile_number} style={{ backgroundColor: '#fff5ee' }} placeholder='Mobile Number' />
+          <Input onChange={setPassword} error={errors?.password} style={{ backgroundColor: '#fff5ee' }} placeholder='Password' />
+
+          <View style={styles.lock_VIEW} >
+            <Image style={styles.lock_image} source={icons.lock} />
+          </View>
+
+          <View style={styles.forgat_Text}>
+            <Paragraph>Forgat Password</Paragraph>
+          </View>
+          <View style={styles.Ematy_Container}></View>
+          <View style={styles.OR_VIEW}>
+            <Paragraph size={12}>OR</Paragraph>
+          </View>
+          <View style={styles.IconsVIEW} >
+            <Image style={styles.facebookImage} source={icons.facebook} />
+            <Image style={styles.googleImage} source={icons.google} />
+          </View>
+          <UiButton text='LOGIN'
+            style={styles.UiButton}
+            onPress={onLogin}
+          />
+          <View style={styles.Register_red}>
+            <Paragraph>Don't have an account ?<Paragraph onPress={() => navigation.navigate('SignUp')} color={'red'} style={styles.Register_red}> Register</Paragraph></Paragraph>
+          </View>
+        </View>
+
       </ScrollContainer>
     </ViewContainer>
   )
@@ -76,12 +131,12 @@ const styles = StyleSheet.create({
   BoxONEContainer: {
     // width: FULL_WIDTH,
     // height:FULL_HEIGHT,
-    height:FULL_HEIGHT*0.43,
+    height: FULL_HEIGHT * 0.43,
     backgroundColor: colors.pink,
     justifyContent: "center",
     alignItems: "center",
-    borderWidth:1,
-    borderBottomRightRadius:300,
+    borderWidth: 1,
+    borderBottomRightRadius: 300,
   },
   ImagePlate: {
     resizeMode: "contain",
@@ -90,14 +145,14 @@ const styles = StyleSheet.create({
   BoxTWOContainer: {
     // width: FULL_WIDTH * 0.90,
     marginHorizontal: 20,
-    marginTop:-60,
+    marginTop: -60,
     borderWidth: 1,
-    borderColor:"blue",
+    borderColor: "blue",
     backgroundColor: colors.white,
     borderRadius: 15,
     elevation: 10,
     opacity: 0.8,
-   
+
 
   },
   LoginText: {
@@ -112,13 +167,13 @@ const styles = StyleSheet.create({
   //  justifyContent:"flex-end",
 
   // },
-  lock_image:{
- position:'absolute',
- bottom:30,
- left:289,
+  lock_image: {
+    position: 'absolute',
+    bottom: 30,
+    left: 289,
   },
   forgat_Text: {
-    right:20,
+    right: 20,
     //  borderWidth:1,
     // width: FULL_WIDTH * 0.50,
     alignSelf: 'flex-end',
@@ -144,11 +199,11 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: 'flex-end',
     alignContent: 'flex-end',
-    marginTop:60,
+    marginTop: 60,
     //  borderWidth:1,
     width: FULL_WIDTH * 0.45,
     marginHorizontal: 37,
-    marginBottom:20,
+    marginBottom: 20,
 
 
   },
--- src/screens/before_login/SignUp.js
+++ src/screens/before_login/SignUp.js
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View ,Image, ScrollView} from 'react-native'
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
 import React, { useState } from 'react'
-import { useRef,useEffect } from 'react';
-import { FULL_HEIGHT, FULL_WIDTH,RADIUS } from '../../constants/layout'
+import { useRef, useEffect } from 'react';
+import { FULL_HEIGHT, FULL_WIDTH, RADIUS } from '../../constants/layout'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import colors from '../../constants/colors'
 import icons from '../../constants/icons'
@@ -11,145 +11,163 @@ import UiButton from '../../components/UI/UiButton'
 import SplashScreen from 'react-native-splash-screen'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { isValidForm, validators } from '../../constants/validation';
-import {POST} from '../../backend/Backend'
-const SignUp = ({navigation}) => {
-const [Email , setEmail]=useState('')
-const [Name , setName]=useState('')
-const [Password , setPassword]=useState('')
-const [ errors , seterrors]=useState('')
-const [loading,setloading]=useState(true)
-const Signvali = async ()=>{
-  const form ={
-    Email:validators.checkEmail('Email',Email),
-    Name:validators.checkRequire('Name',Name),
-    Password:validators.checkPassword('Password',Password),
+import Toast from 'react-native-simple-toast';
+import { POST } from '../../backend/Backend'
+import Loader from '../../components/UI/Loader';
+const SignUp = ({ navigation }) => {
+  const [email, setEmail] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [mobile, setMobile] = useState('')
+  const [errors, seterrors] = useState('')
+  const [loading, setLoading] = useState(true)
 
-  }
-  seterrors(form)
-  if(!isValidForm(form)){
-    let body =  {
-        mobile_number: "+918742817242",
-        first_name:"sakil" ,
-        last_name: "dayma",
-        email: "2343sds@gmail.com",
+  const Signvali = async () => {
+    const form = {
+      email: validators.checkEmail('Email', email),
+      firstName: validators.checkRequire('First Name', firstName),
+      lastName: validators.checkRequire('Last Name', lastName),
+      mobile: validators.checkNumber('Mobile', mobile),
+    }
+    seterrors(form)
+
+    if (isValidForm(form)) {
+      let body = {
+        mobile_number: mobile,
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
         device_information: {
-            device_id: "12345",
-            os_name: "Android",
-            model_name: "SM-M307F",
-            os_version: "10",
-            app_version: "1.3.0",
-            manufacturer: "samsung",
-            total_memory: "5860327424",
-            fcm_token: "12345"
+          device_id: "12345",
+          os_name: "Android",
+          model_name: "SM-M307F",
+          os_version: "10",
+          app_version: "1.3.0",
+          manufacturer: "samsung",
+          total_memory: "5860327424",
+          fcm_token: "12345"
         }
-    } 
-     console.log('body-------->',body);
-     POST(
-      "http://54.144.109.80:5000/api/v1/signup",
-      body,
-      {'Authorization':'Basic YWRtaW46YWRtaW4='},
-      res=>{
-        console.log('res==>',res)
-      },
-      err=>{
-           console.log('err---->',err)
-      },
-      fail=>{
- console.log('fail==>',fail)
       }
-     )
-    // let result = await  fetch('http://54.144.109.80:5000/api/v1/signup',{
-    //   method:'post',
-    //   body:body,
-    //   headers:{
-    //     'Content-Type':'application/json',
-    //     'Authorization':'Basic YWRtaW46YWRtaW4='
-    //   }
-    // })
-    // console.log('res===>',await result.json());
-    
+      console.log('body-------->', body);
+      POST(
+        "http://54.144.109.80:5000/api/v1/signup",
+        body,
+        { 'Authorization': 'Basic YWRtaW46YWRtaW4=' },
+        res => {
+          console.log('res==>', res)
+          if (res.ok) {
+            navigation.navigate("Login")
+
+          } else {
+            Toast.showWithGravity(res.Error.toString(), Toast.LONG, Toast.TOP);
+          }
+        },
+        err => {
+          console.log('err---->', err)
+          let error = JSON.parse(err.res1);
+          Toast.showWithGravity(error.message, Toast.LONG, Toast.TOP);
+        },
+        fail => {
+          console.log('fail==>', fail)
+          Toast.showWithGravity('Api failed');
+        }
+      )
+
+      // let result = await fetch('http://54.144.109.80:5000/api/v1/signup', {
+      //   method: 'post',
+      //   body: body,
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'Authorization': 'Basic YWRtaW46YWRtaW4='
+      //   }
+      // })
+      // console.log('res===>', await result.json());
+
+    } else {
+    }
   }
-}
+
   return (
     <SafeAreaView>
-      
+      <Loader loading={loading} />
       <View style={styles.BoxONEContainer}>
-       <Image style={styles.ImagePlate} source={icons.deliveryboy} />
+        <Image style={styles.ImagePlate} source={icons.deliveryboy} />
+      </View>
+      <View style={styles.BoxTWOContainer}>
+        <View>
+          <Paragraph style={styles.LoginText}>SIGN UP</Paragraph>
         </View>
-        <View  style={styles.BoxTWOContainer}>
-          <View>
-               <Paragraph style={styles.LoginText}>SIGN UP</Paragraph>
-          </View>
-          <Input onChange={setEmail}  error={errors?.Email}style={{backgroundColor:'#fff5ee'}} placeholder='Enter Email id/ Phone No.' />
-          <Input onChange={setName} error={errors?.Name} style={{backgroundColor:'#fff5ee'}} placeholder='Name'/>
-          <Input onChange={setPassword} error={errors?.Password} style={{backgroundColor:'#fff5ee'}} placeholder='Password'/>
+        <Input onChange={setFirstName} error={errors?.firstName} style={{ backgroundColor: '#fff5ee' }} placeholder='First Name' />
+        <Input onChange={setLastName} error={errors?.lastName} style={{ backgroundColor: '#fff5ee' }} placeholder='Last Name' />
+        <Input onChange={setEmail} error={errors?.email} style={{ backgroundColor: '#fff5ee' }} placeholder='Enter Email ' />
+        <Input onChange={setMobile} error={errors?.mobile} style={{ backgroundColor: '#fff5ee' }} placeholder='Mobile Number' />
 
-         <UiButton text='SIGN UP' 
-         style={styles.UiButton}
-         onPress={()=>Signvali()}
-         />
-   <View style={styles.Register_red}>
-   <Paragraph>Don't have an account ?<Paragraph color={'red'} style={styles.Register_red}> Register</Paragraph></Paragraph>
-   </View>
+        <UiButton text='SIGN UP'
+          style={styles.UiButton}
+          onPress={() => Signvali()}
+        />
+        <View style={styles.Register_red}>
+          <Paragraph onPress={() => navigation.navigate("Login")}>Have an account ?<Paragraph color={'red'} style={styles.Register_red}> Login</Paragraph></Paragraph>
         </View>
+      </View>
     </SafeAreaView>
   )
 }
-  
+
 export default SignUp
 
 const styles = StyleSheet.create({
-  BoxONEContainer:{
-    width:FULL_WIDTH,
+  BoxONEContainer: {
+    width: FULL_WIDTH,
     // height:FULL_HEIGHT*0.50,
-    backgroundColor:colors.pink,
-    justifyContent:"center",
-    alignItems:"center",
-    borderBottomLeftRadius:300,
-    
-  },
-  ImagePlate:{
-    resizeMode:"contain",
-    marginBottom:40,
-  },
-  BoxTWOContainer:{
-    height:FULL_HEIGHT*0.45,
-    width:FULL_WIDTH*0.86,
-    marginHorizontal:28,
-    marginVertical:-120,
-   borderWidth:1,
-   backgroundColor:colors.white,
-   borderRadius:9,
-   elevation:10,
-   opacity:0.8,
-   borderBottomLeftRadius:60,
-   borderTopRightRadius:110,
-   borderColor:"blue"
-  
+    backgroundColor: colors.pink,
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomLeftRadius: 300,
 
   },
-  LoginText:{
-   fontSize:30,
-   marginVertical:13,
-   marginHorizontal:10,
+  ImagePlate: {
+    resizeMode: "contain",
+    marginBottom: 40,
   },
-  forgat_Text:{
-  //  borderWidth:1,
-   width:FULL_WIDTH*0.80,
-  alignItems:'flex-end', 
+  BoxTWOContainer: {
+    height: FULL_HEIGHT * 0.45,
+    width: FULL_WIDTH * 0.86,
+    marginHorizontal: 28,
+    marginVertical: -120,
+    borderWidth: 1,
+    backgroundColor: colors.white,
+    borderRadius: 9,
+    elevation: 10,
+    opacity: 0.8,
+    borderBottomLeftRadius: 60,
+    borderTopRightRadius: 110,
+    borderColor: "blue"
+
+
   },
-  UiButton:{
-   backgroundColor:colors.red,
-   alignItems:"center",
-   alignSelf:"center",
-   width:FULL_WIDTH*0.80,
-   padding:15,
-   borderRadius:10,
-   
+  LoginText: {
+    fontSize: 30,
+    marginVertical: 13,
+    marginHorizontal: 10,
   },
-  Register_red:{
-    alignItems:"center",
-    marginVertical:10,
+  forgat_Text: {
+    //  borderWidth:1,
+    width: FULL_WIDTH * 0.80,
+    alignItems: 'flex-end',
   },
- 
+  UiButton: {
+    backgroundColor: colors.red,
+    alignItems: "center",
+    alignSelf: "center",
+    width: FULL_WIDTH * 0.80,
+    padding: 15,
+    borderRadius: 10,
+
+  },
+  Register_red: {
+    alignItems: "center",
+    marginVertical: 10,
+  },
+
 })
